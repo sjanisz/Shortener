7c03b2290a285f3ced51779e6cd8905485655e20
Szymon Janisz <szymon.janisz.et.di@gmail.com>
2021-09-12 09:12:20 +0200

    Small tables refactoring and populating with initial data if tables are empty

--- Shortener.py
+++ Shortener.py
@@ -3,9 +3,11 @@ from models import *
 from views import *
 
 def create_tables():
-    db.create_tables([Verb, Adjective, Noun, Map], safe=True)
+    db.create_tables([Verb, Adjective, Noun, Map], safe=True) 
 
 if __name__ == '__main__':
     db.connect()
     create_tables()
+    PopulateTablesWithDummyData()
+    
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,20 +1,21 @@
 from peewee import *
 from app import db
+from DatabaseInitializationData import *
 
 class Verb(Model):
-    verb = CharField()
+    name = CharField(unique=True)
 
     class Meta:
         database = db
 
 class Adjective(Model):
-    adjective = CharField()
+    name = CharField(unique=True)
 
     class Meta:
         database = db
 
 class Noun(Model):
-    noun = CharField()
+    name = CharField(unique=True)
 
     class Meta:
         database = db
@@ -27,3 +28,23 @@ class Map(Model):
 
     class Meta:
         database = db
+
+def PopulateTablesWithDummyData():
+    _populateVerbTableWithDummyData()
+    _populateAdjectiveTableWithDummyData()
+    _populateNounTableWithDummyData()
+
+def _populateVerbTableWithDummyData():
+    if(Verb.select().count() == 0):
+        for verb in VerbInitData:
+            Verb(name=verb).save()
+
+def _populateAdjectiveTableWithDummyData():
+    if(Adjective.select().count() == 0):
+        for adjective in AdjectiveInitData:
+            Adjective(name=adjective).save()
+
+def _populateNounTableWithDummyData():
+    if(Noun.select().count() == 0):
+        for noun in NounInitData:
+            Noun(name=noun).save()
